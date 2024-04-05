bd04d1bd9d0553fc5c64e114ba6137f29aa2d110
deniskacaj <denkacaj@gmail.com>
2024-04-05 13:10:08 +0300

    feat: styles cards

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,7 +1,8 @@
-import {Image, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {styles} from './cards.styles.ts';
 import Nike from '../../../public/assets/icons/Nike-logo.svg';
 import FavoriteLogo from '../../../public/assets/icons/Favorite.svg';
+import StarEmptyLogo from '../../../public/assets/icons/star-empty.svg';
 import nikeMain from '../../../public/assets/images/nikeMain.png';
 import React from 'react';
 
@@ -13,6 +14,21 @@ export const Cards = () => {
         <FavoriteLogo width={24} height={24} fill={'none'} />
       </View>
       <Image style={styles.img} source={nikeMain} />
+      <View style={styles.inner}>
+        <View style={styles.box}>
+          <Text style={styles.title}>Nike Air Max Plus |||</Text>
+          <View style={styles.stars}>
+            <StarEmptyLogo width={17} height={15} fill={'gold'} />
+            <StarEmptyLogo width={17} height={15} fill={'gold'} />
+            <StarEmptyLogo width={17} height={15} fill={'gold'} />
+            <StarEmptyLogo width={17} height={15} fill={'gold'} />
+            <StarEmptyLogo width={17} height={15} fill={'lightgrey'} />
+          </View>
+        </View>
+        <Text style={styles.priceAmount}>
+          $<Text style={styles.priceValue}>189</Text>
+        </Text>
+      </View>
     </View>
   );
 };
