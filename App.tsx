012cf75d8caa4d6e17384270281eb77e7f182936
deniskacaj <denkacaj@gmail.com>
2024-04-09 09:34:18 +0300

    feat: add icons header

--- App.tsx
+++ App.tsx
@@ -1,72 +1,81 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {MainScreen} from './src/screens/main';
 import {CardScreen} from './src/screens/card';
 import {RootStackParamList} from './src/types/navigation.types.ts';
+import Hamburger from './assets/icons/Hamburger.svg';
+import Basket from './assets/icons/Basket.svg';
 
 export const App = () => {
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
   return (
-    <>
-      <SafeAreaView style={styles.safeArea}></SafeAreaView>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={'Main'}
-          screenOptions={({route}) => ({
-            headerStyle: {
-              backgroundColor: route.name === 'Main' ? '#e5e5e5' : 'white',
-            },
-            headerTransparent: false,
-            headerSearchBarOptions:
-              route.name === 'Main'
-                ? {
-                    textColor: '#8a8a9d',
-                    barTintColor: 'white',
-                    hideNavigationBar: false,
-                    placeholder: 'Search...',
-                  }
-                : undefined,
-          })}>
-          <Stack.Screen
-            name="Main"
-            component={MainScreen}
-            options={{
-              title: 'Explore',
-              headerTitleStyle: styles.headerTitleMain,
-            }}
-          />
-          <Stack.Screen
-            name="Card"
-            component={CardScreen}
-            options={{
-              title: 'Running',
-              headerTitleStyle: styles.headerTitleCard,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={'Main'}
+        screenOptions={({route}) => ({
+          headerStyle: {
+            backgroundColor: route.name === 'Main' ? '#e5e5e5' : 'white',
+          },
+          headerTransparent: false,
+          headerSearchBarOptions:
+            route.name === 'Main'
+              ? {
+                  textColor: '#8a8a9d',
+                  barTintColor: 'white',
+                  hideNavigationBar: false,
+                  placeholder: 'Search...',
+                }
+              : undefined,
+          headerLeft: () => (
+            <TouchableOpacity>
+              <Hamburger width={24} height={24} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity>
+              <Basket width={24} height={24} />
+            </TouchableOpacity>
+          ),
+        })}>
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{
+            title: 'Explore',
+            headerTitleStyle: styles.headerTitleMain,
+          }}
+        />
+        <Stack.Screen
+          name="Card"
+          component={CardScreen}
+          options={{
+            title: 'Running',
+            headerTitleStyle: styles.headerTitleCard,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  safeArea: {
-    backgroundColor: '#e5e5e5',
-  },
   headerTitleMain: {
     fontSize: 21,
     fontFamily: 'OpenSans-Bold',
     color: '#313B5D',
-    backgroundColor: 'red',
   },
   headerTitleCard: {
     fontSize: 21,
     fontFamily: 'OpenSans-Bold',
     color: '#313B5D',
-    backgroundColor: 'red',
+  },
+  burgerIcon: {
+    marginLeft: 10,
+  },
+  cartIcon: {
+    marginRight: 10,
   },
 });
